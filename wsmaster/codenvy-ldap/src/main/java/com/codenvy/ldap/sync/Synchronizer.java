a08d6c43a9cdc6d9a6814060a7e5b04486e1c1e0
Yevhenii Voevodin <evoevodin@codenvy.com>
2016-09-12 10:21:05 +0300

    Fix build

--- wsmaster/codenvy-ldap/src/main/java/com/codenvy/ldap/sync/Synchronizer.java
+++ wsmaster/codenvy-ldap/src/main/java/com/codenvy/ldap/sync/Synchronizer.java
@@ -1,3 +1,17 @@
+/*
+ *  [2012] - [2016] Codenvy, S.A.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Codenvy S.A. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Codenvy S.A.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Codenvy S.A..
+ */
 package com.codenvy.ldap.sync;
 
 import org.eclipse.che.api.user.server.model.impl.ProfileImpl;
@@ -95,6 +109,7 @@ public class Synchronizer {
         try (Connection connection = connFactory.getConnection()) {
             connection.open();
             for (LdapEntry entry : selector.select(connection)) {
+
                 syncCount++;
             }
         }
